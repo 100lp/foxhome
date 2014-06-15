8f9f1958f2f460c300f0a3b9d8a69b70ef03b0b1
100lp <spb.gurme@gmail.com>
2014-06-16 01:21:07 +0400

    finish helper for left_nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
       link_to link_text, link_path
     end
   end
+  def second_nav_link(link_text, link_path)
+	  class_name = request.fullpath.include?(link_path) && link_path != root_path ? 'current' : ''
+	  class_name = current_page?(link_path) ? 'current' : class_name
+
+  	link_to link_path, :class => class_name do
+	  	content_tag(:li) do
+	      link_text
+	    end
+	  end
+	end
 end
