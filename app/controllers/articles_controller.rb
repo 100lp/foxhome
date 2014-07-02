f6dab8ff656257fa4b1cbf6dce4723ad71296d9c
100lp <spb.gurme@gmail.com>
2014-07-02 09:32:39 +0400

    ok

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,6 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
     @current_page = request.env['PATH_INFO']
     @body_bg = ["default", "first", "second"]
-    @vk_nav = [ pressa_path, classes_path, doings_path, contact_path ]
+    @vk_nav = [ pressa_path, classes_path, doings_path, contacts_path ]
   end
 end
