48cb129f40a9024ffde90ac566e71b6eec378b2b
100lp <spb.gurme@gmail.com>
2014-07-28 20:48:38 +0400

    ppc

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,16 @@ Rails.application.routes.draw do
   # post 'contact' => 'contact#create'
   
   # navigation
-  get '/about', to: 'pages#about'
   get '/interiors', to: 'pages#interiors'
+  get '/mebel', to: 'pages#mebel'
   get '/grafika', to: 'pages#grafika'
+
+  get '/portfolio', to: 'pages#portfolio'
+  get '/uslugi', to: 'pages#uslugi'
+  get '/price', to: 'pages#price'
+  get '/learning', to: 'pages#learning'
+  get '/articles', to: 'pages#articles'
+  get '/about', to: 'pages#about'
   get '/contacts', to: 'pages#contacts'
 
   # left navigation
