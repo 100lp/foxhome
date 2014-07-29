948016ea66836620e169b4d4a05eb9ffd54fc528
100lp <spb.gurme@gmail.com>
2014-07-29 13:42:30 +0400

    something

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,28 @@
 module ApplicationHelper
+
 	def nav_link(link_text, link_path)
     class_name = request.fullpath.include?(link_path) && link_path != root_path ? 'current' : ''
     class_name = current_page?(link_path) ? 'current' : class_name
 
-    content_tag(:li, :class => class_name) do
-      link_to link_text, link_path
-    end
+    link_to link_text, link_path, :class => class_name
   end
   def second_nav_link(link_text, link_path)
 	  class_name = request.fullpath.include?(link_path) && link_path != root_path ? 'current' : ''
 	  class_name = current_page?(link_path) ? 'current' : class_name
 
-  	link_to link_path, :class => class_name do
-	  	content_tag(:li) do
-	      link_text
-	    end
-	  end
+  	content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+	end
+
+	def english_link(link_text, link_path)
+		class_name = request.fullpath.include?('/en') ? 'active' : ''
+
+  	link_to link_text, link_path, :class => class_name
+	end
+	def russian_link(link_text, link_path)
+		class_name = request.fullpath.include?('/en') ? '' : 'active'
+
+  	link_to link_text, link_path, :class => class_name
 	end
 end
